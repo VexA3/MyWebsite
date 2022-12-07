5b7591ae9ce8e5b371bd8bd665343a6114848e27
Billy <47019848+VexA3@users.noreply.github.com>
2022-12-06 18:14:49 -0600

    replcae apostrophe

--- pages/about.tsx
+++ pages/about.tsx
@@ -16,7 +16,7 @@ export default function About() {
           <h2 className={styles.h2}>My name is Billy</h2>
           <h3 className={styles.h3}>This is my website</h3>
           <h3 className={styles.h3}>Its currently a work in progress.</h3>
-          <p className={styles.p}>Please come back at a later time to see what I've got planned.</p>
+          <p className={styles.p}>Please come back at a later time to see what I&aposve got planned.</p>
           {/* <p className={styles.p}>Your name was  right?</p> */}
         </div>
       </main>
