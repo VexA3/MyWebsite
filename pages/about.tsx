11c87307d53d6544548f3ce9fb3bb32be92f9fd4
Billy <47019848+VexA3@users.noreply.github.com>
2022-12-24 13:55:46 -0600

    updated homepage css

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import React from 'react';
 import styles from '../styles/About.module.css'
 import Footer from './footer'
 
@@ -17,7 +18,7 @@ export default function About() {
           <h3 className={styles.h3}>This is my website</h3>
           <h3 className={styles.h3}>Its currently a work in progress.</h3>
           <p className={styles.p}>Please come back at a later time to see what I&aposve got planned.</p>
-          {/* <p className={styles.p}>Your name was  right?</p> */}
+          {/* <p className={styles.p}>Your name was right?</p> */}
         </div>
       </main>
       <Footer/>
